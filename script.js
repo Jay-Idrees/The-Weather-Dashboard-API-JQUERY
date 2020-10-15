8c1b66810f245eb6f544311339b2d137dcd67ae6
Jay Idrees <idreesjay@gmail.com>
2020-10-15 08:06:46 -0400

    Pseudocoding: Outlined Event Sequence, variables and functions

--- script.js
+++ script.js
@@ -20,3 +20,51 @@
      // Log the resulting object
      console.log(response);
     });
+
+
+    //===========================================
+
+         //Event Sequence 
+
+   //===============================================
+
+    // Capture the city from userinput search into a variable
+
+    // Triger a click event on user entring the city name and clicking
+
+              // Adds city to the search list
+
+              // Updats the Ajax query URL based on the  city searched
+
+              // Updates Jumbotron info
+
+              // updates 5-day weather forcast info
+
+//=================================
+
+     // Variables for functions             
+
+//=================================
+
+                // Capture variables of date, temp (F), and humidity
+
+                var city=response.name
+
+                var date= response.dt
+
+                console.log(date);
+
+                var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+
+                var humidity=response.main.humidity
+
+                // Weather icon
+                var iconcode = a.weather[0].icon;
+
+                var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+
+//====================================
+ 
+// Functions for Event Sequence
+
+//==========================================
